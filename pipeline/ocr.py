506cb09f2aadba8ae3941b1b47e63b2b748a46bd
khala1391 <khala1391@gmail.com>
2026-05-24 01:43:30 +0700

    update

--- pipeline/ocr.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from PIL import Image
-
-
-def run_ocr(image: Image.Image, reader) -> str:
-    img_array = np.array(image.convert("RGB"))
-    results = reader.readtext(img_array, detail=0, paragraph=True)
-    return "\n".join(results)
